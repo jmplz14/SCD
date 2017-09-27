e36db405b59ebfd55c5948860dc88d821ffa2587
jmplz14 <jmplz14@correo.ugr.es>
2017-09-27 15:59:37 +0200

    seminario 1 primer ejercicio

--- seminario1/ejemplo09-plantilla.cpp
+++ seminario1/ejemplo09-plantilla.cpp
@@ -43,32 +43,50 @@ double calcular_integral_secuencial(  )
 // función que ejecuta cada hebra: recibe $i$ ==índice de la hebra, ($0\leq i<n$)
 double funcion_hebra( long i )
 {
-   // completar ......
+   double suma_parcial = 0.0 ; 
+	int numero_pasadas = m/n, fin_bucle = i * numero_pasadas + numero_pasadas ;
+	
+   for ( long j = i * numero_pasadas ; j < fin_bucle ; j++ )
+		suma_parcial += f( (j+double(0.5)) /m );
+		
+	return suma_parcial;
 }
 
 // -----------------------------------------------------------------------------
 // calculo de la integral de forma concurrente
 double calcular_integral_concurrente( )
 {
-  // completar .....
+	
+	future <double> futuros[n];
+	double suma_total = 0.0;
+	for ( long i = 0 ; i < n ; i++ ){
+		
+		futuros[i] = async ( launch::async, funcion_hebra, i);
+	}
+	
+	for ( long i = 0 ; i < n ; i++ )
+		suma_total += futuros[i].get();
+	
+	return suma_total/m;
 }
 // -----------------------------------------------------------------------------
 
 int main()
 {
-
   time_point<steady_clock> inicio_sec  = steady_clock::now() ;
   const double             result_sec  = calcular_integral_secuencial(  );
   time_point<steady_clock> fin_sec     = steady_clock::now() ;
+   
   double x = sin(0.4567);
   time_point<steady_clock> inicio_conc = steady_clock::now() ;
+ 
   const double             result_conc = calcular_integral_concurrente(  );
   time_point<steady_clock> fin_conc    = steady_clock::now() ;
   duration<float,milli>    tiempo_sec  = fin_sec  - inicio_sec ,
                            tiempo_conc = fin_conc - inicio_conc ;
   const float              porc        = 100.0*tiempo_conc.count()/tiempo_sec.count() ;
 
-
+	
   constexpr double pi = 3.14159265358979323846l ;
 
   cout << "Número de muestras (m)   : " << m << endl
