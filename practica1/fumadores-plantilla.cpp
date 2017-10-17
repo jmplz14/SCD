4862200ad6f0e41cadaf7eae4dc415a1650cdcc8
jmplz14 <jmplz14@correo.ugr.es>
2017-10-17 15:23:05 +0200

    Terminado el ejericio de los fumadores y la practica 1

--- practica1/fumadores-plantilla.cpp
+++ practica1/fumadores-plantilla.cpp
@@ -19,7 +19,9 @@ using namespace SEM ;
 // distribuido entre dos valores enteros, ambos incluidos
 // (ambos tienen que ser dos constantes, conocidas en tiempo de compilación)
 //----------------------------------------------------------------------
-
+const unsigned int NUMERO_FUMADORES = 3;
+Semaphore estanquero = 1;
+Semaphore fumadores[NUMERO_FUMADORES] = {0,0,0};
 template< int min, int max > int aleatorio()
 {
   static default_random_engine generador( (random_device())() );
@@ -29,10 +31,27 @@ template< int min, int max > int aleatorio()
 
 //----------------------------------------------------------------------
 // función que ejecuta la hebra del estanquero
-
+int producir(){
+		unsigned int num_objeto = aleatorio<0,2>();
+		chrono::milliseconds duracion_producir(aleatorio<20,200>());
+		this_thread::sleep_for(duracion_producir);
+		
+		
+		return num_objeto;
+	
+}
 void funcion_hebra_estanquero(  )
 {
-
+	unsigned int num_objeto;
+	while(true){
+		sem_wait(estanquero);
+		num_objeto = producir();
+		cout << "producido ingrediente:" << num_objeto << endl;
+		sem_signal(fumadores[num_objeto]);
+		
+		
+	}
+	
 }
 
 //-------------------------------------------------------------------------
@@ -62,10 +81,12 @@ void fumar( int num_fumador )
 // función que ejecuta la hebra del fumador
 void  funcion_hebra_fumador( int num_fumador )
 {
-   while( true )
-   {
-
-   }
+	while(true){
+		sem_wait(fumadores[num_fumador]);
+		cout << "retirado ingrediente: " << num_fumador << endl;
+		sem_signal(estanquero);
+		fumar(num_fumador);
+	}
 }
 
 //----------------------------------------------------------------------
@@ -74,4 +95,12 @@ int main()
 {
    // declarar hebras y ponerlas en marcha
    // ......
+	thread hebra_estanquero(funcion_hebra_estanquero);
+	thread hebra_fumadores[NUMERO_FUMADORES];
+	for(int i = 0; i < NUMERO_FUMADORES; i++)
+		hebra_fumadores[i]= thread(funcion_hebra_fumador, i);
+		
+	hebra_estanquero.join();
+	for(int i = 0; i < NUMERO_FUMADORES; i++)
+		hebra_fumadores[i].join();
 }
